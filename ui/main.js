a80674eee6cdb246083c500b1a2f260a48517eb6
Rohan Shah <37570371+rohanshah95@users.noreply.github.com>
2018-03-27 11:49:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element= document.getElementById('main-text');
 element.innerHTML = "New Value";
+
+var image = document.getElementById('img');
+img.onclick=function()
+{
+    img.style.marginleft='100px';
+};
