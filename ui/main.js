36975b18bf1641d12e965659243f220784436c08
Rohan Shah <37570371+rohanshah95@users.noreply.github.com>
2018-03-28 03:08:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,22 @@
 var button=document.getElementById('counter');
-var counter=0;
 button.onclick=function()
 {
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    //CREATE A REQUEST
+    var request=new XMLHttpRequest();
+    //CAPTURE
+    request.onreadystatechange=function()
+    {
+        if(request.readystate===XMLHttpRequest.DONE)
+        {
+            if(request.stature===200)
+            {
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+  //MAKE A REQUEST
+  request.open('GET', 'http://rohanshah3895rs.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
